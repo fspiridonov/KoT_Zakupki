e9d211c5c0679e431ed45703e78ab02207132e2f
fspiridonov <spiridonov_fl@mgtniip.ru>
2018-05-14 11:27:15 +0300

    [+] Увеличен таймер

--- src/test/java/ru/zakupki/Pages/ProcurementPage.java
+++ src/test/java/ru/zakupki/Pages/ProcurementPage.java
@@ -86,7 +86,7 @@ public class ProcurementPage {
     public static void clickvaluation() throws IOException {
 //клик на поле Расчет НМЦК
         WebElement name = $(By.xpath("//label[text()='Расчет НМЦК']"));
-        sleep(1600);
+        sleep(1700);
 
         try {
             name.isDisplayed();
