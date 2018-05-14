84252ef28c6dfee21a189815391e9131ca09504a
Boriskin_SA <Boriskin_SA@mgtniip.ru>
2018-05-14 14:14:48 +0300

    test

--- src/test/java/ru/zakupki/Pages/ProcurementPage.java
+++ src/test/java/ru/zakupki/Pages/ProcurementPage.java
@@ -86,7 +86,7 @@ public class ProcurementPage {
     public static void clickvaluation() throws IOException {
 //клик на поле Расчет НМЦК
         WebElement name = $(By.xpath("//label[text()='Расчет НМЦК']"));
-        sleep(1500);
+        sleep(1800);
 
         try {
             name.isDisplayed();
